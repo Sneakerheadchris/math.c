b0af21343736cceab7f73b35253bdf39b6060bbd
Sneakerheadchris <47801724+Sneakerheadchris@users.noreply.github.com>
2019-05-01 22:02:34 -0400

    Update math.c

--- math.c
+++ math.c
@@ -8,25 +8,25 @@ int main(){
     printf("press 1 to find voltage, press 2 to find resistance, press 3 to find current\n");
     scanf("%d", &option);
     if(option==1){
-        printf("what's your resistance?\n");
+        printf("enter resistance\n");
         scanf("%f", &r);
-        printf("what's your current?\n");
+        printf("enter current\n");
         scanf("%f", &i);
         result=r*i;
             printf("the voltage is %0.2f\n", result);
     }
     else  if(option==2){
-        printf("what's your voltage?\n");
+        printf("enter voltage\n");
         scanf("%f", &v);
-        printf("what's your current?\n");
+        printf("enter current\n");
         scanf("%f", &i);
         result=v/i;
             printf("the resistance is %0.2f\n", result);
 }
     else  if(option==3){
-        printf("what's your voltage?\n");
+        printf("enter voltage\n");
         scanf("%f", &v);
-        printf("what's your resistance?\n");
+        printf("enter resistance\n");
         scanf("%f", &r);
         result=v/r;
             printf("the current is %0.2f\n", result);
